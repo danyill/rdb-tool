b9604a1ff829808e82a1653a2f664335bf2a15d9
Daniel Mulholland <dan.mulholland@gmail.com>
2015-05-01 08:09:28 +1200

    first properly working version ;-)

--- rdbextract.py
+++ rdbextract.py
@@ -13,6 +13,19 @@ This tool can be run from the IDLE prompt using the start def, e.g.
 start('-h') or start('start.rdb G1:50P1P')
 
 Thoughtful ideas most welcome. 
+
+Installation instructions (for Python *2.7.9*:
+ - pip install tablib
+ - or if behind a proxy server: pip install --proxy="user:password@server:port" packagename
+ - within Transpower: pip install --proxy="transpower\mulhollandd:password@tptpxy001.transpower.co.nz:8080" tablib    
+
+TODO: 
+ - include settinsg which parameter is used in:
+  print "used in: " 
+  print re.findall('^' + SEL_SETTING_NAME + ",\"" + 
+                   SEL_EXPRESSION + sys.argv[1] + SEL_EXPRESSION 
+                   + "\"" + SEL_SETTING_EOL, 
+                   rdat, flags=re.MULTILINE) 
 """
 
 __author__ = "Daniel Mulholland"
@@ -29,16 +42,15 @@ import os
 import argparse
 import glob
 import re
+import tablib
 
-RDB_EXTENSION = '.rdb'
+RDB_EXTENSION = 'RDB'
 INPUT_FOLDER = "in"
-BASE_PATH = os.path.join(os.path.dirname(os.path.realpath(__file__)),INPUT_FOLDER)
-# this needs to be a character which can't be used in file RDB naming and
-# SEL don't use it within their filename structure.
-RDB_SEPARATOR = '/'
-SEL_expression = r'[\w :+/\\()!,.-_\\*]*'
-SEL_setting_EOL = r'\x1c\r\n'
-SEL_setting_name = r'[\w _]*'
+BASE_PATH = os.path.dirname(os.path.realpath(__file__))
+SEL_EXPRESSION = r'[\w :+/\\()!,.\-_\\*]*'
+SEL_SETTING_EOL = r'\x1c\r\n'
+SEL_SETTING_NAME = r'[\w _]*'
+SEL_FID_EXPRESSION='^FID=([\w :+/\\()!,.\-_\\*]*)\r\n'
 
 from thirdparty.OleFileIO_PL import OleFileIO_PL
 
@@ -48,10 +60,9 @@ def get_ole_data(filename):
         ole = OleFileIO_PL.OleFileIO(filename)
         listdir = ole.listdir()
         for direntry in listdir:            
-            #print direntry
             data.append([direntry, ole.openstream(direntry).getvalue()])
     except:
-        print 'Failed to read streams.'
+        print 'Failed to read streams in file: ' + filename
     return data
 
 def main(arg=None):
@@ -61,7 +72,7 @@ def main(arg=None):
         epilog='Enjoy. Bug reports and feature requests welcome. Feel free to build a GUI :-)',
         prefix_chars='-/')
 
-    parser.add_argument('-o', choices=['csv','xls'],
+    parser.add_argument('-o', choices=['csv','xlsx'],
                         help='Produce output as either comma separated values (csv) or as'\
                         ' a Micro$oft Excel .xls spreadsheet. If no output provided then'\
                         ' output is to the screen.')
@@ -72,6 +83,9 @@ def main(arg=None):
                        ' searches for files with:' +  RDB_EXTENSION + '. Globbing is'\
                        ' allowed with the * and ? characters.')
 
+    parser.add_argument('-s', '--screen', action="store_true",
+                       help='Show output to screen')
+                       
     parser.add_argument('-d', '--design', action="store_true",
                        help='Attempt to determine Transpower standard design version and' \
                        ' include this information in output')
@@ -95,32 +109,41 @@ def main(arg=None):
         args = parser.parse_args()
     else:
         args = parser.parse_args(arg.split())
-
-    path_result = glob.glob(os.path.join(BASE_PATH,args.path[0]))
-
-    files_to_do = return_file_paths(path_result, RDB_EXTENSION)
+    
+    files_to_do = return_file_paths(args.path, RDB_EXTENSION)
+    
     if files_to_do != []:
-        # print(files_to_do)
         process_rdb_files(files_to_do, args)
     else:
-        print('Found nothing to do')
+        print('Found nothing to do for path: ' + args.path[0])
         sys.exit()
 
 def process_rdb_files(files_to_do, args):
     parameter_info = []
+    
     for filename in files_to_do:      
         # print filename
         rdb_info = get_ole_data(filename)
-        # print rdb_info
-        #parameter_info = extract_parameters(filename, rdb_info, args)
-        #print parameter_info
-        #display_info(parameter_info)
-        parameter_info += extract_parameters(filename, rdb_info, args)
-    #print parameter_info
-    results_for_display = []
-    #for k in parameter_info:
-    #    results_for_display.join(k)
-    display_info(parameter_info)
+        extracted_data = extract_parameters(filename, rdb_info, args)
+        parameter_info += extracted_data
+
+    # for exporting to Excel or CSV
+    data = tablib.Dataset()    
+    for k in parameter_info:
+        data.append(k)
+    data.headers = ['File','Name','Setting','RDB','Value','FID']
+
+    if args.o == None:
+        pass
+    elif args.o == 'csv':
+        with open('output.csv','wb') as output:
+            output.write(data.csv)
+    elif args.o == 'xlsx':
+        with open('output.xlsx','wb') as output:
+            output.write(data.xlsx)
+
+    if args.screen == True:
+        display_info(parameter_info)
 
 def display_info(parameter_info):
     lengths = []
@@ -132,7 +155,7 @@ def display_info(parameter_info):
             except IndexError:
                 lengths.append(len(element))
     
-    parameter_info.insert(0,['File','Name','Setting','RDB','Val'])
+    parameter_info.insert(0,['RDB File','Name','Setting File','Setting Name','Val'])
     # now display in columns            
     for line in parameter_info:
         display_line = '' 
@@ -152,68 +175,26 @@ def extract_parameters(filename, rdb_info, args):
             if len(stream[0]) >= 3:
                 return_value = extract_parameter_from_stream(parameter,\
                     stream[1])
+                # print stream
+                fid = extract_fid(stream[1])
                 if return_value <> []:
                     filename = os.path.basename(filename)
                     settings_name = str(stream[0][1])
                     stream_name = str(stream[0][-1])
                     parameter_info.append([filename, settings_name,\
-                        stream_name, parameter, return_value[0]])
+                        stream_name, parameter, return_value[0], fid[0]])
     return parameter_info
 
-def extract_parameter_from_stream(parameter,stream):
-      return re.findall('^' + parameter + \
-            ",\"(" + SEL_expression + ")\"" + \
-            SEL_setting_EOL, \
-            stream, flags=re.MULTILINE)
+def extract_fid(stream):
+    # FIDs look like this for example:
+    return re.findall(SEL_FID_EXPRESSION, \
+        stream, flags=re.MULTILINE)
 
-def return_file_paths(path_result, file_extension):
-    files_to_do = []
-    # make a list of files to iterate over
-    if path_result != None:
-        for p_or_f in path_result:
-            if os.path.isfile(p_or_f) == True:
-                # add file to the list
-                files_to_do.append(os.path.realpath(p_or_f))
-            elif os.path.isdir(p_or_f) == True:
-                # walk about see what we can find
-                files_to_do += walkabout(p_or_f, file_extension)
-    return files_to_do
-    
-if __name__ == '__main__':
-    main('*.rdb IN101D IN102D IN103D IN104D')
-
-def arra():
-  rdat = 'a'
-  myfile = open('output/combined.txt', 'r')
-  with myfile as f:
-    rdat = f.read()
-
-  print(rdat)
-
-  
-  #  print re.findall(pattern, string, flags)
-  # print re.findall('^'+sys.argv[1]+'.*\r\n', rdat, flags=re.MULTILINE)
-
-  # this is for finding named settings of sys.argv[1]
-  print "variable of the name: " + sys.argv[1]
-
-  variable_name = re.findall('^' + sys.argv[1] + ",\"([\w :+/()!,.-]*)\"\x1c\r\n", rdat, flags=re.MULTILINE)
-  variable_name = re.findall('^' + sys.argv[1] + 
-                             ",\"(" + SEL_expression + ")\"" + 
-                             SEL_setting_EOL, 
-                             rdat, flags=re.MULTILINE)
-
-  print sys.argv[1] + '=' + str(variable_name)
-  # SV1,"(51P1T+67P1T+67N1T+IN206+IN208*LT8+SV1)*50P5"
-  print "used in: " 
-
-  print re.findall('^' + SEL_setting_name + ",\"" + 
-                   SEL_expression + sys.argv[1] + SEL_expression 
-                   + "\"" + SEL_setting_EOL, 
-                   rdat, flags=re.MULTILINE) 
-
-  myfile.close()
-
+def extract_parameter_from_stream(parameter,stream):      
+    return re.findall('^' + parameter + \
+        ",\"(" + SEL_EXPRESSION + ")\"" + \
+        SEL_SETTING_EOL, \
+        stream, flags=re.MULTILINE)
 
 def walkabout(p_or_f, file_extension):
     """ searches through a path p_or_f, picking up all files with EXTN
@@ -221,9 +202,35 @@ def walkabout(p_or_f, file_extension):
     """
     return_files = []
     for root, dirs, files in os.walk(p_or_f, topdown=False):
-        for name in files:        
-            if os.path.basename(p_or_f)[0:-3] == file_extension:
-                return_files.append(os.path.realpath(os.path.join(root,name)))
-                print("Found: " + name)
-    print('here you are sir')
-    print return_files
+        #print files
+        for name in files:
+            if (os.path.basename(name)[-3:]).upper() == file_extension:
+                return_files.append(os.path.join(root,name))
+    return return_files
+    
+def return_file_paths(args_path, file_extension):
+    paths_to_work_on = []
+    for p in args_path:
+        p = p.translate(None, ",\"")
+        if not os.path.isabs(p):
+            paths_to_work_on +=  glob.glob(os.path.join(BASE_PATH,p))
+        else:
+            paths_to_work_on += glob.glob(p)
+            
+    files_to_do = []
+    # make a list of files to iterate over
+    if paths_to_work_on != None:
+        for p_or_f in paths_to_work_on:
+            if os.path.isfile(p_or_f) == True:
+                # add file to the list
+                files_to_do.append(os.path.normpath(p_or_f))
+            elif os.path.isdir(p_or_f) == True:
+                # walk about see what we can find
+                files_to_do = walkabout(p_or_f, file_extension)
+    return files_to_do
+    
+if __name__ == '__main__':
+    #main(r'-o xlsx W:/Education/Current/20150430_Stationware_Settings_Issued/SNI IN101[?] TR')
+    #main(r'-o xlsx "/media/alexandria/Education/Current/20150430_Stationware_Settings_Issued/SNI/" IN101[?] TR')
+    main(r'-o xlsx "/media/alexandria/Education/Current/20150430_Stationware_Settings_Applied/" TR')
+    #main(r'-o xlsx in IN101[?] TR')
